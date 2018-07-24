85b6cd78abf055437441bb08cedacf73b8ce82cc
Kento75 <kento2github@gmail.com>
2018-07-24 21:16:02 +0900

    add: books scaffolding

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -6,4 +6,12 @@ class HelloController < ApplicationController
   def view
     @msg = 'こんにちは、世界！'
   end
+
+  def list
+    @books = Book.all
+  end
+
+  def app_var
+    render plain: MY_APP['logo']['source']
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_115144) do
+ActiveRecord::Schema.define(version: 2018_07_24_114353) do
 
   create_table "books", force: :cascade do |t|
     t.string "isbn"
